844d62dc87f5fe1249b2ea3c2372ab9e78be05f2
DelioAraujo <delioaraujo@gmail.com>
2023-08-03 12:04:17 -0300

    req2 ok

--- src/routes/talker.routes.js
+++ src/routes/talker.routes.js
@@ -3,6 +3,7 @@ const readFile = require('../readFile');
 
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 
 // requisito 1
 talkerRoutes.get('/', async (req, res) => {
@@ -10,4 +11,16 @@ talkerRoutes.get('/', async (req, res) => {
     res.status(HTTP_OK_STATUS).json(file);
   });
 
+// requisito 2
+talkerRoutes.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const info = await readFile();
+    const infoFiltered = info.find((person) => person.id === Number(id));
+    if (infoFiltered) {
+        return res.status (HTTP_OK_STATUS).json(infoFiltered)
+    }
+    return res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+})
+
   module.exports = talkerRoutes;
+
