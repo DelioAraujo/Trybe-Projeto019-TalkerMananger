19565addf1620b052dada8211a296fa9919628e4
DelioAraujo <delioaraujo@gmail.com>
2023-08-03 13:38:07 -0300

    req3 ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const talkerRoutes = require('./talker.routes');
+const loginRoutes = require ('./login.routes')
 
 router.use('/talker', talkerRoutes);
+router.use('/login', loginRoutes)
 
 module.exports = router;
---
+++ src/routes/login.routes.js
@@ -0,0 +1,16 @@
+const loginRoutes = require('express').Router();
+const crypto = require('crypto');
+
+const HTTP_OK_STATUS = 200;
+
+// requisito 3
+loginRoutes.post('/', (req, res) => {
+
+  // Gera um token aleatório de 16 caracteres
+  const token = crypto.randomBytes(8).toString('hex');
+
+  // Retorna o token gerado no corpo da resposta
+  res.status(HTTP_OK_STATUS).json({ token });
+});
+
+module.exports = loginRoutes;
